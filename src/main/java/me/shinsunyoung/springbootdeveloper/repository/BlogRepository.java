c6ab3a564caa5add35abfb4779f16e135f0b0127
why-not-available <aever144@naver.com>
2024-06-14 17:39:41 +0900

    6-2 리포지터리 만들기

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/repository/BlogRepository.java
@@ -0,0 +1,9 @@
+package me.shinsunyoung.springbootdeveloper.repository;
+// JpaRepository클래스를 상속받을때 엔티티 Article와 엔티티의 PK타입 Long을 인수로 넣는다
+
+import me.shinsunyoung.springbootdeveloper.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article, Long> {
+
+}
