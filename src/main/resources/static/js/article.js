82b799e6559008d38b079f003e1f98c4ebcd0a18
why-not-available <aever144@naver.com>
2025-02-26 18:07:21 +0900

    7.4 삭제기능 추가하기 (2)실행테스트**삭제가 안됨** 해결: 백틱(``) 이슈 + disable cache 설정하기

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -4,7 +4,8 @@ const deleteButton = document.getElementById('delete-btn');
 if(deleteButton){
     deleteButton.addEventListener('click', event => {
         let id = document.getElementById('article-id').value;
-        fetch('/api/articles/${id}', {
+        console.log("삭제할 ID: ", id);
+        fetch(`/api/articles/${id}`, {
             method: 'DELETE'
         })
         .then(() => {
