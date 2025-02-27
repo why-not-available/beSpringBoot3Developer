8eafa7a52eee3cb96cd370d05fdf2dd5efaf6759
why-not-available <aever144@naver.com>
2025-02-27 18:24:39 +0900

    7.5 수정/생성 기능 추가하기 (2)뷰 만들기 (3)실행

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -14,3 +14,29 @@ if(deleteButton){
         });
     });
 }
+
+// 수정하기
+const modifyButton = document.getElementById('modify-btn'); //: id가 'modify-btn'인 엘레먼트 조회하기
+
+if(modifyButton){
+    //: click 이벤트가 감지되면 수정API 요청
+    modifyButton.addEventListener('click', event => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`/api/articles/${id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value
+            })
+        })
+        .then(() => {
+            alert('수정이 완료되었습니다.');
+            location.replace(`/articles/${id}`);
+        });
+    });
+}
