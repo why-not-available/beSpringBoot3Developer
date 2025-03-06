aa634f61c0f54c432fcd31b4b088df3f440a260c
why-not-available <aever144@naver.com>
2025-03-06 16:01:15 +0900

    8.6 로그아웃 구현하기 (1) 로그아웃 메서드 추가 (2) 로그아웃 뷰 추가

--- src/main/java/me/shinsunyoung/springbootdeveloper/controller/UserApiController.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/controller/UserApiController.java
@@ -1,18 +1,32 @@
 package me.shinsunyoung.springbootdeveloper.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.shinsunyoung.springbootdeveloper.dto.AddUserRequest;
 import me.shinsunyoung.springbootdeveloper.service.UserService;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class UserApiController {
     private final UserService userService;
 
+    // 회원가입
     public String signup(AddUserRequest request){
         userService.save(request);
         return "redirect:/login"; //: redirect:는 회원가입 처리 후 로그인 페이지로 이동하기
+    }
+
+    // 로그아웃
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 
 }
