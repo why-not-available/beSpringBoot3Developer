137067c69b7aa4430f634100da9e815b4746fe59
why-not-available <aever144@naver.com>
2025-03-07 00:25:54 +0900

    8.7 실행테스트 (2) 로그인, 회원가입 실행테스트 ** 회원가입부터 안되는듯(DB에 값이 안들어감) > UserApi컨트롤로에 @PostMapping("/user") 빼먹음

--- src/main/java/me/shinsunyoung/springbootdeveloper/controller/UserApiController.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/controller/UserApiController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -17,6 +18,7 @@ public class UserApiController {
     private final UserService userService;
 
     // 회원가입
+    @PostMapping("/user")
     public String signup(AddUserRequest request){
         userService.save(request);
         return "redirect:/login"; //: redirect:는 회원가입 처리 후 로그인 페이지로 이동하기
